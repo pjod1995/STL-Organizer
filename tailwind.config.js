37b44ab0a8201fade10ce20079a72aef9be7bf3d
pjod1995 <pjod1995@gmail.com>
2026-05-17 23:58:43 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,1 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export default {content:['./index.html','./src/**/*.{js,jsx}'],theme:{extend:{}},plugins:[],};
